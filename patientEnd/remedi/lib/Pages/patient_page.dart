c906c831bbab046d45cc9e6ab0aa2495cd134c80
Zarif Ikram <65703522+zarifikram@users.noreply.github.com>
2021-11-26 15:16:25 +0600

    Update patient_page.dart
    dummy buttons added for debug

--- patientEnd/remedi/lib/Pages/patient_page.dart
+++ patientEnd/remedi/lib/Pages/patient_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import "package:velocity_x/velocity_x.dart";
 import '../api_caller.dart';
+import '../person.dart';
 
 class PatientPage extends StatefulWidget{
   @override
@@ -53,4 +54,88 @@ class _PatientPageState extends State<PatientPage> {
     },
     future: ResponseAPICall,//ResponseAPICall,
   );
+
+  Widget GetDataColumn(BuildContext context, AsyncSnapshot<Object?> snapshot) {
+    var response = snapshot.data! as Map<String, dynamic>;
+    print(response);
+    var tests = response["tests"];
+    List<dynamic> docs = response["specialists"];
+    List<Widget> testButtons = [];
+    List<ExpansionPanel> docButtons = [];
+
+    for(var test in tests){
+      var button = MaterialButton(
+        color: context.cardColor.withOpacity(0.3),
+        textColor: context.cardColor,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        onPressed: (){},
+        child: (test as String).text.bold.center.xl2.make().box.width(context.percentWidth*70).make().py12(),
+      ).box.rounded.makeCentered().py12();
+      testButtons.add(button);
+    }
+
+    for(var doc in docs){
+      int index = docs.indexOf(doc);
+      String id = doc["id"];
+      String email = doc["email"];
+      String name = doc["name"];
+      String address = doc["address"];
+      var card = ExpansionPanel(
+          canTapOnHeader: true,
+          backgroundColor: context.canvasColor,
+          headerBuilder: (context, isOpen){
+            return name.text.xl3.make();
+          },
+          isExpanded: isOpens[index],
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  email.text.xl.make(),
+                  Icon(Icons.mail_rounded),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  address.text.xl.make(),
+                  Icon(Icons.pin_drop)
+                ],
+              ),
+              AppointButton(context, id),
+            ],
+          ).box.rounded.height(300).makeCentered().p12()
+      );
+      docButtons.add(card);
+    }
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          "Recommended Tests".text.xl4.bold.make().py16(),
+          ...testButtons,
+          ProceedButton(context, "Order Tests", (){
+            (Navigator.push(context, MaterialPageRoute(builder: (context)=>PatientPage())));
+          }),
+          "Recommended ${Person.specialization} in ${Person.city.firstLetterUpperCase()}".text.xl4.bold.make().py16(),
+          ExpansionPanelList(
+            expansionCallback: (index, isExpanded){setState(() {isOpens[index] = !isExpanded;});},
+            elevation: 0,
+            dividerColor: context.canvasColor,
+            children: docButtons,
+          ),
+        ],
+      ).px12(),
+    ).expand();
+  }
+
+  Widget ProceedButton(BuildContext context, String text, Null Function() param2) => Container();
+
+  Widget AppointButton(BuildContext context, String id) => Container();
+
+
+
 }
