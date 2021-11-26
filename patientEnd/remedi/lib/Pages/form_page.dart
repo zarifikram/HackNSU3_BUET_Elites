c1faa6a40657ce4752354a875722be44a5c4c470
Zarif Ikram <65703522+zarifikram@users.noreply.github.com>
2021-11-26 11:45:01 +0600

    Update form_page.dart
    need custom text field and drop down

--- patientEnd/remedi/lib/Pages/form_page.dart
+++ patientEnd/remedi/lib/Pages/form_page.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
 import "package:velocity_x/velocity_x.dart";
+
+import 'chat_page.dart';
 class FormPage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     List<String> ages = [];
     for(int i = 1; i < 101; i++) ages.add(i.toString());
     List<String> cities = ["Bronx","Brooklyn","Cedarhurst","Flushing","Jackson Heights","Manhattan","New York","Queens","Staten Island"];
-    return Container().box.red600.rounded.make();
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: context.canvasColor,
+        body: Form(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                "First tell us a bit about yourself.".text.xl4.bold.make().py16(),
+                //custom input and dropdown needed
+                
+                MaterialButton(
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                  onPressed: () => (Navigator.push(context, MaterialPageRoute(builder: (context)=>ChatPage()))),
+                  child: "Proceed".text.color(context.cardColor).xl4.make().px64().py16(),
+                  color: context.cardColor.withOpacity(.3),
+                ).py32(),
+              ],
+            ).px12(),
+          ),
+        ),
+      ),
+    );
   }
 
 }
