0632d5c75bdc764ee37b4e01645f7c73acf40b18
Zarif Ikram <65703522+zarifikram@users.noreply.github.com>
2021-11-26 12:17:46 +0600

    custom text field working
    form will be updated later

--- patientEnd/remedi/lib/Pages/form_page.dart
+++ patientEnd/remedi/lib/Pages/form_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:remedi/Widgets/custom_text_field.dart';
 import "package:velocity_x/velocity_x.dart";
 
 import 'chat_page.dart';
@@ -14,16 +15,19 @@ class FormPage extends StatelessWidget{
         body: Form(
           child: SingleChildScrollView(
             child: Column(
+
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                "First tell us a bit about yourself.".text.xl4.bold.make().py16(),
+                Container().box.make().py16(),
+                "First tell us a bit about yourself.".text.xl2.bold.make().py16(),
                 //custom input and dropdown needed
-                
+                CustomTextField(inputHint: "e.g zarif" ,label: "Name",),
                 MaterialButton(
                   shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
                   onPressed: () => (Navigator.push(context, MaterialPageRoute(builder: (context)=>ChatPage()))),
                   child: "Proceed".text.color(context.cardColor).xl4.make().px64().py16(),
                   color: context.cardColor.withOpacity(.3),
-                ).py32(),
+                ).py32().box.makeCentered(),
               ],
             ).px12(),
           ),
---
+++ patientEnd/remedi/lib/Widgets/custom_drop_item.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class CustomDropItem extends StatefulWidget{
+  @override
+  State<CustomDropItem> createState() => _CustomDropItemState();
+}
+
+class _CustomDropItemState extends State<CustomDropItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
