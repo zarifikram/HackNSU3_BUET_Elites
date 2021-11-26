fe27c5e2245f433b0699722e9c10582a17de5fec
Zarif Ikram <65703522+zarifikram@users.noreply.github.com>
2021-11-26 11:22:53 +0600

    Update person.dart
    for form use

---
+++ patientEnd/remedi/lib/person.dart
@@ -0,0 +1,13 @@
+class Person{
+  static String name = "zarif";
+  static String sex = "male";
+  static int age = 10;
+  static String key = "fe117fac-5b48-4ba4-9c19-10f775f8665b";
+  static String address = "116, Dhanmondi, Dhaka";
+  static String height = "1.7";
+  static String weight = "73";
+  static String street = "fasdf";
+  static String phone = "adfas";
+  static String city = "Bronx";
+  static String doctorId = "hehe";
+}
