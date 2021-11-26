101b5887e06675efcb5aa7c7ccd23d5e7465c3e7
SalmanSayeed <echoonlineclasses@gmail.com>
2021-11-26 11:33:15 +0600

    Completed critical and auth page basic UI

--- doctor-end/src/Pages/Patients.js
+++ doctor-end/src/Pages/Patients.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import {Box,Typography} from '@mui/material'
-
+import PatientCard from '../Components/PatientCard'
+const patient={
+    name:"Salman Sayeed",
+    age:"21",
+    height:"1.8",
+    weight:"65",
+    emergency:true
+}
 export default function Patients() {
     return (
         <Box sx={{width:"100vw",minHeight:"100vh",display:"flex",flexDirection:"column",alignItems:"center",justifyContent:"flex-start"}}>
@@ -8,7 +15,18 @@ export default function Patients() {
                 <Typography variant="h2" sx={{color:"white"}}>Patients Page</Typography>
                 <Typography sx={{color:"white"}}>All the patients the you are currently supervising</Typography>
             </Box>
-            
+            <Box sx={{width:"80vw",marginTop:"-10vh",minHeight:"100vh",display:"flex", alignItems:"flex-start", justifyContent:"center", flexWrap:"wrap"}}>
+                <PatientCard data={patient}/>
+                <PatientCard data={patient}/>
+                <PatientCard data={patient}/>
+                <PatientCard data={patient}/>
+                <PatientCard data={patient}/>
+                <PatientCard data={patient}/>
+                <PatientCard data={patient}/>
+                <PatientCard data={patient}/>
+                <PatientCard data={patient}/>
+                <PatientCard data={patient}/>
+            </Box>
         </Box>
     )
 }
