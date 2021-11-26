30b3ae0182077d1850d35ef268627c13fa670302
Zarif Ikram <65703522+zarifikram@users.noreply.github.com>
2021-11-26 14:53:22 +0600

    Update patient_page.dart
    init updated. that calls the api

--- patientEnd/remedi/lib/Pages/patient_page.dart
+++ patientEnd/remedi/lib/Pages/patient_page.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/cupertino.dart';
 
-class PatientPage extends StatelessWidget{
+import '../api_caller.dart';
+
+class PatientPage extends StatefulWidget{
+  @override
+  State<PatientPage> createState() => _PatientPageState();
+}
+
+class _PatientPageState extends State<PatientPage> {
+  List<bool> isOpens = [];
+  late Future<Map<String, dynamic>> ResponseAPICall;
+  late Future<Map<String, dynamic>> ResponseAPICall2;
+
+  @override
+  void initState() {
+    ResponseAPICall = API_CALLER.DIAGNOSIS();
+    for(int i = 0; i < 100; i++) isOpens.add(false);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
   }
-
 }
